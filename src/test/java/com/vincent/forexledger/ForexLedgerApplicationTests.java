9f88cf9de6cf149dab522af4f2cb05d1d670d41c
Vincent <eric61011@gmail.com>
2021-08-09 23:29:05 +0800

    Initialize project.

---
+++ src/test/java/com/vincent/forexledger/ForexLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincent.forexledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForexLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
