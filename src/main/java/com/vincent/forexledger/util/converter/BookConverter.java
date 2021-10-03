e0726fbf2415d30b0261bb567d94e081a5c8145d
Vincent <eric61011@gmail.com>
2021-10-03 12:51:32 +0800

    Prepare converting method from book to list resonse. Provide unit test.

--- src/main/java/com/vincent/forexledger/util/converter/BookConverter.java
+++ src/main/java/com/vincent/forexledger/util/converter/BookConverter.java
@@ -1,7 +1,11 @@
 package com.vincent.forexledger.util.converter;
 
 import com.vincent.forexledger.model.book.Book;
+import com.vincent.forexledger.model.book.BookListResponse;
 import com.vincent.forexledger.model.book.CreateBookRequest;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class BookConverter {
     private BookConverter() {
@@ -17,5 +21,23 @@ public class BookConverter {
         book.setProfitRate(null);
 
         return book;
+    }
+
+    public static List<BookListResponse> toBookListResponses(List<Book> books) {
+        return books.stream()
+                .map(BookConverter::toBookListResponse)
+                .collect(Collectors.toList());
+    }
+
+    public static BookListResponse toBookListResponse(Book book) {
+        var response = new BookListResponse();
+        response.setId(book.getId());
+        response.setName(book.getName());
+        response.setCurrencyType(book.getCurrencyType());
+        response.setBalance(book.getBalance());
+        response.setTwdProfit(book.getTwdProfit());
+        response.setProfitRate(book.getProfitRate());
+
+        return response;
     }
 }
