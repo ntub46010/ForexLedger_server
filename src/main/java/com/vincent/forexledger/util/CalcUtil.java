b774524fca9d21c69efc016127a9108d91d4e7dd
Vincent <eric61011@gmail.com>
2021-10-18 00:00:21 +0800

    Implement add and subtract operation.

--- src/main/java/com/vincent/forexledger/util/CalcUtil.java
+++ src/main/java/com/vincent/forexledger/util/CalcUtil.java
@@ -7,6 +7,23 @@ public class CalcUtil {
     private CalcUtil() {
     }
 
+    public static double addToDouble(double num1, BigDecimal num2) {
+        return BigDecimal.valueOf(num1)
+                .add(num2)
+                .doubleValue();
+    }
+
+    public static BigDecimal subtractToDecimal(double num1, double num2) {
+        return BigDecimal.valueOf(num1)
+                .subtract(BigDecimal.valueOf(num2));
+    }
+
+    public static double subtractToDouble(double num1, BigDecimal num2) {
+        return BigDecimal.valueOf(num1)
+                .subtract(num2)
+                .doubleValue();
+    }
+
     public static int multiplyToInt(double num1, double num2) {
         if (num1 == 0 || num2 == 0) {
             return 0;
@@ -26,5 +43,15 @@ public class CalcUtil {
                 .setScale(digit, RoundingMode.HALF_DOWN)
                 .divide(BigDecimal.valueOf(num2), RoundingMode.HALF_DOWN)
                 .doubleValue();
+    }
+
+    public static BigDecimal divideToDecimal(BigDecimal num1, int num2, int digit) {
+        if (BigDecimal.ZERO.equals(num1)) {
+            return BigDecimal.ZERO;
+        }
+
+        return num1
+                .setScale(digit, RoundingMode.HALF_DOWN)
+                .divide(BigDecimal.valueOf(num2), RoundingMode.HALF_DOWN);
     }
 }
--- src/test/java/com/vincent/forexledger/unit/CalcUtilTests.java
+++ src/test/java/com/vincent/forexledger/unit/CalcUtilTests.java
@@ -4,7 +4,24 @@ import com.vincent.forexledger.util.CalcUtil;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 public class CalcUtilTests {
+
+    @Test
+    public void testAddToDouble() {
+        Assert.assertEquals(27.9985, CalcUtil.addToDouble(27.9685, BigDecimal.valueOf(0.03)), 0);
+    }
+
+    @Test
+    public void testSubtractToDouble() {
+        Assert.assertEquals(28.0385, CalcUtil.subtractToDouble(28.0685, BigDecimal.valueOf(0.03)), 0);
+    }
+
+    @Test
+    public void testSubtractToDecimal() {
+        Assert.assertEquals(27.9705, CalcUtil.subtractToDecimal(28.0005, 0.03).doubleValue(), 0);
+    }
 
     @Test
     public void testMultiplyToInt() {
@@ -19,5 +36,14 @@ public class CalcUtilTests {
         Assert.assertEquals(38.2458, CalcUtil.divideToDouble(3000, 78.44, 4), 0); // 38.24579
         Assert.assertEquals(-0.0150, CalcUtil.divideToDouble(-359, 23877, 4), 0); // -0.01503
         Assert.assertEquals(0.0194, CalcUtil.divideToDouble(1882, 97148, 4), 0); // 0.01937
+    }
+
+    @Test
+    public void testDivideToDecimal() {
+        var result = CalcUtil.divideToDecimal(BigDecimal.valueOf(0), 5, 4).doubleValue();
+        Assert.assertEquals(0.0, result, 0);
+
+        result = CalcUtil.divideToDecimal(BigDecimal.valueOf(0.48), 5, 4).doubleValue();
+        Assert.assertEquals(0.096, result, 0);
     }
 }
